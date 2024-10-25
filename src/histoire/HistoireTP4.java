09a18abc16cac75fe2572bb0cd80de6f9ab1cc5b
walid0999 <walid.kadfi-athmani@univ-tlse3.fr>
2024-10-25 09:36:49 +0200

    Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,17 +2,20 @@ package histoire;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof", "kombucha", 54);
+//		Humain prof = new Humain("Prof", "kombucha", 54);
 //		prof.direBonjour();
 //		prof.acheter("une boisson", 12);
 //		prof.boire();
 //		prof.acheter("un jeu", 2);
 //		prof.acheter("un kimono", 50);
-		Commercant marco = new Commercant("Marco", 0);
-		marco.direBonjour();
-		marco.seFaireExtorquer();
-		marco.recevoir(15);
-		marco.boire();
+		Commercant marco = new Commercant("Marco", 15);
+//		marco.direBonjour();
+//		marco.seFaireExtorquer();
+//		marco.recevoir(15);
+//		marco.boire();
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "Whisky", 30, "Warsong");
+		yaku.direBonjour();
+		yaku.extorquer(marco);
 	}
 
 }
---
+++ src/histoire/Yakuza.java
@@ -0,0 +1,25 @@
+package histoire;
+
+public class Yakuza extends Humain {
+	private String clan;
+	private int reputation = 0;
+
+	public Yakuza(String nom, String boisson_pref, int argent, String clan ) {
+		super(nom, boisson_pref, argent);
+		this.clan = clan;
+	}
+	
+	public void extorquer(Commercant victime) {
+		int argent_gagné = victime.getArgent();
+		gagnerArgent(argent_gagné);
+		System.out.println("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ? ");
+		System.out.println(victime.getNom()+", si tu tiens à la vie donne moi ta bourse !");
+		victime.seFaireExtorquer();
+		this.clan += 1;
+		System.out.println("J'ai piqué "+argent_gagné+" sous de "+victime.getNom()+
+				", ce qui me fait "+getArgent()+" sous dans ma poche. Hi ! Hi !");
+		
+	}
+	
+
+}
